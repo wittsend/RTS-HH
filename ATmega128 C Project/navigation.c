3607fe1b5d9da5445689e71ce12f7723e7d395c0
Matt Witt <wittsend@users.noreply.github.com>
2018-03-15 10:39:14 +1300

    Add navigation and motion function modules from template

---
+++ ATmega128 C Project/navigation.c
@@ -0,0 +1,43 @@
+/*
+* navigation.c
+*
+* Author : Matthew Witt (wittsend86@gmail.com)
+* Created: 15/03/2018 10:37:41 AM
+*
+* Project Repository: https://github.com/wittsend/RTS-HH
+*
+* 1 or 2 liner on the purpose of the file
+*
+* More Info:
+* Atmel ATmega128 Datasheet:http://ww1.microchip.com/downloads/en/DeviceDoc/doc2467.pdf
+* Relevant reference materials or datasheets if applicable
+*
+* Functions:
+* void funcName(void)
+*
+*/
+
+//////////////[Includes]////////////////////////////////////////////////////////////////////////////
+#include "navigation.h"
+
+//////////////[Private Defines]/////////////////////////////////////////////////////////////////////
+
+//////////////[Private Global Variables]////////////////////////////////////////////////////////////
+
+//////////////[Functions]///////////////////////////////////////////////////////////////////////////
+/*
+* Function: 
+* [function declaration]
+*
+* [brief purpose of function]
+*
+* Inputs:
+* [input arguments and any relevant explanation]
+*
+* Returns:
+* [return values and any relevant explanation]
+*
+* Improvements:
+* [Ideas for improvements that are yet to be made](optional)
+*
+*/
---
+++ ATmega128 C Project/navigation.h
@@ -0,0 +1,32 @@
+/*
+* navigation.h
+*
+* Author : Matthew Witt (wittsend86@gmail.com)
+* Created: 15/03/2018 10:37:54 AM
+*
+* Project Repository: https://github.com/wittsend/RTS-HH
+*
+* 1 or 2 liner on the purpose of the file
+*
+* More Info:
+* Atmel ATmega128 Datasheet:http://ww1.microchip.com/downloads/en/DeviceDoc/doc2467.pdf
+* Relevant reference materials or datasheets if applicable
+*
+* Functions:
+* void funcName(void)
+*
+*/
+
+#ifndef NAVIGATION_H_
+#define NAVIGATION_H_
+
+//////////////[Includes]////////////////////////////////////////////////////////////////////////////
+
+//////////////[Public Defines]//////////////////////////////////////////////////////////////////////
+
+//////////////[External Global Variables]///////////////////////////////////////////////////////////
+
+//////////////[Functions]///////////////////////////////////////////////////////////////////////////
+
+
+#endif /* NAVIGATION_H_ */
